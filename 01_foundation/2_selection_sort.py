3cbf1d523a34998978d8076a04ce3e7d20cc8e88
incipient1 <anbangnihao@gmail.com>
2018-02-28 19:30:26 +0800

    4th

---
+++ 01_foundation/2_selection_sort.py
@@ -0,0 +1,19 @@
+def selection_sort(a):
+    for i in range(len(a)-1):
+        min_a = a[i]
+        min_index = i
+        for j in range(i+1,len(a)):
+            if a[j] < min_a:
+                min_a = a[j]
+                min_index = j
+            else:
+                pass
+
+        a[i], a[min_index] = min_a, a[i]
+
+    return a
+
+if __name__ == '__main__':
+    a = [89,32,12,5,6,9,45,19,20,20,16]
+    print(a)
+    print(selection_sort(a))
